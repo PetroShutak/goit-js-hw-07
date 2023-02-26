f04770ce866122947dbcd356b13c552b8268fca4
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-02-26 11:30:20 +0200

    add second way to first picture

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -5,6 +5,11 @@ export const galleryItems = [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
+    // preview:
+    //   'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg',
+    // original:
+    //   'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    // description: 'Hokkaido Flower',
   },
   {
     preview:
